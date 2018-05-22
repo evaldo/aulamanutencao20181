9ef5b811f34089118a5cc6a17c5813966b387ab7
Raian <Raian@falaserio.com.br>
2018-05-21 22:39:29 -0300

    A new comiitng here

--- Hello.java
+++ Hello.java
@@ -3,6 +3,12 @@ public class HelloWorld {
   public static void main(String[] args) {
       HelloWorld hello = new HelloWorld();
       hello.imprimeTexto("Raian Henrique");
+      HelloWorld hello = new HelloWorld();
+      hello.imprimeTexto("Raian Henrique");
+      HelloWorld hello = new HelloWorld();
+      hello.imprimeTexto("Raian Henrique");
+      HelloWorld hello = new HelloWorld();
+      hello.imprimeTexto("Raian Henrique");
   }
  
   public void imprimeTexto(String texto){
